1f4ca10b16a27ef74e66ebb97043e23252deb9a4
SapoTurco <garridofranciscojose01@gmail.com>
2026-04-21 18:50:38 -0500

    actualizar CORS

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # CORS (para frontend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://sapoturco.github.io/iub-frontend/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
