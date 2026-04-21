a6230fd88fcbe8088a50847d33c2c9357730a03f
SapoTurco <garridofranciscojose01@gmail.com>
2026-04-21 18:40:40 -0500

    actualizar db

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
-import mysql.connector
+import os
 
 def get_db():
     return mysql.connector.connect(
-        host="mysql.railway.internal",
-        port=3306,
-        user="root",
-        password="dzVNsdxaLHxLfZRQHbKBKifHaujuebIF",
-        database="railway"
+        host=os.environ.get("MYSQL_HOST"),
+        port=int(os.environ.get("MYSQL_PORT", 3306)),
+        user=os.environ.get("MYSQL_USER"),
+        password=os.environ.get("MYSQL_PASSWORD"),
+        database=os.environ.get("MYSQL_DATABASE")
     )
