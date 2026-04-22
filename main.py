7e693faa02c1f97d4d85e55b2b759275e041cc96
SapoTurco <garridofranciscojose01@gmail.com>
2026-04-21 23:53:06 -0500

    test CORS wildcard

--- main.py
+++ main.py
@@ -4,15 +4,10 @@ from api.routes import router
 
 app = FastAPI()
 
-# CORS (para frontend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://sapoturco.github.io",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
