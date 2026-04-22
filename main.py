dc97bfc3b0d870172800018e7de92f395f932414
SapoTurco <garridofranciscojose01@gmail.com>
2026-04-21 23:48:43 -0500

    fix CORS github pages

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ app = FastAPI()
 # CORS (para frontend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://sapoturco.github.io",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
