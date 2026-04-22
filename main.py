d38df4b6ac25d74e7ccb9249074320fe5ed690a5
SapoTurco <garridofranciscojose01@gmail.com>
2026-04-21 19:11:08 -0500

    cors updated

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ app = FastAPI()
 # CORS (para frontend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://sapoturco.github.io",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
